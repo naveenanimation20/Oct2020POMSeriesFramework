bbbb430f14cb95ac51ad597e8267f3bcef73d8d7
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-01-15 07:54:42 +0530

    update and deleted some files

---
+++ src/main/java/com/qa/opencart/pages/DealsPage.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class DealsPage {
+	
+	public void testDeal() {
+		System.out.println("added deals page...");
+	}
+	
+	
+
+}
--- src/main/java/com/qa/opencart/pages/ProfilePage.java
+++ src/main/java/com/qa/opencart/pages/ProfilePage.java
@@ -1,7 +1,9 @@
+package com.qa.opencart.pages;
+
 public class ProfilePage{
 
   ProfilePage(){
-    
+    System.out.println("added profile page.....");
   }
 
 
