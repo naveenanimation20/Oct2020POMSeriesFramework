8788cab06d6082d1f1a0a94a55e023ef00eb92d9
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-01-16 09:07:14 +0530

    updated signup as per review comments

--- src/main/java/com/qa/opencart/pages/SignUpPage.java
+++ src/main/java/com/qa/opencart/pages/SignUpPage.java
@@ -1,12 +1,17 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class SignUpPage {
+	
+	
+	private By signup = By.id("singup");
 	
 	public SignUpPage() {
 		System.out.println("this is sign up page const....");
 	}
 	
-	public void doSignUP() {
+	public void signUpProcess() {
 		System.out.println("do sign up....");
 	}
 	
