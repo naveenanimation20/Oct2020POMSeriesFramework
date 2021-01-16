9c8dd9cdece139d7a5a3ceee9041ef6180bc876f
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-01-16 08:54:34 +0530

    updated productInfoPage and created Signup page

---
+++ src/main/java/com/qa/opencart/pages/SignUpPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class SignUpPage {
+	
+	public SignUpPage() {
+		System.out.println("this is sign up page const....");
+	}
+	
+	public void doSignUP() {
+		System.out.println("do sign up....");
+	}
+	
+	
+
+}
