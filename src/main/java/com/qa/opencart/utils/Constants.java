1cbd3657c06f845fa5c6e8e50c79eaea18b4f482
naveenautomationlabs <naveenautomationlabs@naveenautomationlabss-MacBook-Pro.local>
2021-01-19 07:56:09 +0530

    fixed the acc page header name in constants

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
-	public static final String ACCOUNTS_PAGE_HEADER = "Your Store11";
+	public static final String ACCOUNTS_PAGE_HEADER = "Your Store";
 	public static final int ACCOUNTS_PAGE_SECTIONS_COUNT = 4;
 
 	public static final String ACCOUNT_SUCCESS_MESSG = "Your Account Has Been Created!";
